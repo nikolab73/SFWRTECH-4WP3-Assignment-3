aefb794f58651f1128e7f0b7b93d97e1ac1051ed
Nikola Bogdanovic <bogdanon@mcmaster.ca>
2026-03-14 16:31:37 -0400

    added delete functionality through controller routing and bug fixes

--- controllers/editors.js
+++ controllers/editors.js
@@ -9,4 +9,15 @@ router.get("/", async function(req, res) {
     res.render("editors", req.TPL);
 });
 
+router.get("/deleteArticle/:title/:username", async function(req, res) {
+    await ArticlesModel.deleteArticle(req.params.title, req.params.username);
+    res.redirect("/editors");
+});
+
+router.get("/deleteUser/:username", async function(req, res) {
+    await ArticlesModel.deleteByUser(req.params.username);
+    await UsersModel.deleteUser(req.params.username);
+    res.redirect("/editors");
+});
+
 module.exports = router;
