b19d0d7c8aeb3eb0f99bf5ca35e2029e172ec73b
Nikola Bogdanovic <bogdanon@mcmaster.ca>
2026-03-14 15:58:08 -0400

    bringing users to editors page

--- controllers/editors.js
+++ controllers/editors.js
@@ -1,10 +1,12 @@
 const express = require('express');
-var router = express.Router()
+var router = express.Router();
+const UsersModel = require('../models/users.js');
+const ArticlesModel = require('../models/articles.js');
 
-// Display the editors page
-router.get("/", async function(req, res)
-{
-  res.render("editors", req.TPL);
+router.get("/", async function(req, res) {
+    req.TPL.users = await UsersModel.getAllUsers();
+    req.TPL.articles = await ArticlesModel.getAllArticles();
+    res.render("editors", req.TPL);
 });
 
 module.exports = router;
